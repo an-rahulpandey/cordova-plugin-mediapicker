d2e5c669eddf7c9b99a8cf836ef3cfe1e3512ff6
Rahul Pandey <rahul.p@applicationnexus.com>
2016-03-07 16:32:27 +0530

    Added Error Callback on User Cancellation. #8

--- src/ios/MediaPicker.m
+++ src/ios/MediaPicker.m
@@ -228,6 +228,8 @@
 
 - (void)mediaPickerDidCancel:(MPMediaPickerController *)mediaPicker
 {
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Selection cancelled"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackID];
     [self.viewController dismissViewControllerAnimated:YES completion:nil];
 }
 
